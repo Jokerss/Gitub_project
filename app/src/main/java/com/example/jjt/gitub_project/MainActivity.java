0926b2b403ffc3a72c21c1a6d9f46858e21a9bc8
Jokersjtj <1003293767@qq.com>
2016-06-20 18:04:24 +0800

    实现log

--- app/src/main/java/com/example/jjt/gitub_project/MainActivity.java
+++ app/src/main/java/com/example/jjt/gitub_project/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.jjt.gitub_project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("123","this is hehe");
+
 
     }
 }
